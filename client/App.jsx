06d3e07ac630b1cade6af5400f6be83403668b1c
fraisai <r.uddin292@gmail.com>
2022-07-26 13:41:04 -0400

    created components and containers for frontend and added Mantine cards

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import MainContainer from './containers/MainContainer.jsx';
 
 const App = () => (
   <div id="app">
-      <h1>App</h1>
+      <h1>Job Tracker</h1>
+      <MainContainer/>
   </div>
 );
 
---
+++ client/containers/ColumnContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Column from '../components/Column.jsx'
+
+const ColumnContainer = () => {
+  return (
+    <div style={{width: '80%', margin: '0 auto'}}>
+      <Column />
+    </div>
+  );
+}
+
+export default ColumnContainer
