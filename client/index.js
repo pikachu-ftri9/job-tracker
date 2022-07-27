c823b9675f66d09997aa3f8916c3b4a5288ea8ee
fraisai <r.uddin292@gmail.com>
2022-07-27 18:41:52 -0400

    usecontextprovider implemented in index.js

--- client/index.js
+++ client/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
+import { UserContext, UserContextProvider } from './UserContext.jsx'
+
 // import store from './store.js';
 import './scss/styles.scss';
 
-render(<App />, document.getElementById('root'));
+render(<UserContextProvider><App /></UserContextProvider>
+, document.getElementById('root'));
